c896beeb6ef8c1f6bf25e16c478955d554e9b4fd
MaksBoik0 <145847193+MaksBoik0@users.noreply.github.com>
2023-10-21 14:41:08 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from logic import gen_pass
+
+#ctrl+shift+P
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+    
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+    
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(15))
+
+
+bot.run("ТОКЕН")
